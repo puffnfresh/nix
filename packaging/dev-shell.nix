a77f89e5058ff2a40e5655c17ddaf6581e3da983
Brian McKenna <brian@brianmckenna.org>
2025-02-05 10:49:01 +1100

    Don't include nixfmt on Windows
    Doesn't cross-compile to Windows for some reason. This unblocks
    Windows progress.

--- packaging/dev-shell.nix
+++ packaging/dev-shell.nix
@@ -117,8 +117,8 @@ pkgs.nixComponents.nix-util.overrideAttrs (
         pkgs.buildPackages.changelog-d
         modular.pre-commit.settings.package
         (pkgs.writeScriptBin "pre-commit-hooks-install" modular.pre-commit.settings.installationScript)
-        inputs.nixfmt.packages.${pkgs.hostPlatform.system}.default
       ]
+      ++ lib.optional (!stdenv.hostPlatform.isWindows) inputs.nixfmt.packages.${pkgs.hostPlatform.system}.default
       # TODO: Remove the darwin check once
       # https://github.com/NixOS/nixpkgs/pull/291814 is available
       ++ lib.optional (stdenv.cc.isClang && !stdenv.buildPlatform.isDarwin) pkgs.buildPackages.bear
