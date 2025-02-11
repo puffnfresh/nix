5afd338e1dae4a0ea147fbdd28a42169cffd339b
Brian McKenna <brian@brianmckenna.org>
2025-02-11 20:25:07 +1100

    Add some Nix code to help with debugging in VSCode

---
+++ packaging/launch.nix
@@ -0,0 +1,65 @@
+{ stdenv
+, lib
+, gcc
+, gdb
+, wine64Packages
+, writeText
+}:
+
+let
+  program =
+    "\${workspaceFolder}/build/src/nix/nix.exe";
+  launch =
+    {
+      version = "0.2.0";
+      inputs = [
+        {
+          id = "args";
+          type = "promptString";
+          description = "Arguments to nix.exe";
+        }
+      ];
+      configurations = [
+        {
+          name = "winedbg";
+          type = "cppdbg";
+          request = "launch";
+          cwd = "\${workspaceFolder}";
+          inherit program;
+
+          MIMode = "gdb";
+          miDebuggerPath = "${lib.getBin gdb}/bin/${stdenv.targetPlatform.config}-gdb";
+          miDebuggerServerAddress = "localhost:9939";
+          debugServerPath = lib.getExe' wine64Packages.minimal "winedbg";
+          debugServerArgs = "--gdb --no-start --port 9939 ${program} \${input:args}";
+
+          # HACK: I don't know why "target remote" doesn't work.
+          serverStarted = "Could not create tray window";
+          filterStderr = true;
+
+          setupCommands = [
+            {
+              description = "Pretty-printing";
+              text = "-enable-pretty-printing";
+              ignoreFailures = false;
+            }
+            {
+              description = "Pretty-printing for libstdc++";
+              text = builtins.replaceStrings ["\n"] ["; "] ''
+                python sys.path.insert(0, '${lib.getLib gcc.cc}/share/gcc-${gcc.cc.version}/python')
+                from libstdcxx.v6.printers import register_libstdcxx_printers
+                register_libstdcxx_printers(None)
+              '';
+              ignoreFailures = false;
+            }
+            {
+              description = "Do not stop for SIGABRT (exceptions)";
+              text = "handle SIGABRT nostop noprint noignore";
+              ignoreFailures = false;
+            }
+          ];
+        }
+      ];
+    };
+in
+writeText "launch.json" (builtins.toString (builtins.toJSON launch))
--- scripts/copy-dlls.sh
+++ scripts/copy-dlls.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
 
-copied=1
-while [ "$copied" -ne 0 ]; do
-  copied=0
-  for i in src/nix/{*.exe,lib*.dll}; do
-    for j in $("/mnt/d/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/bin/Hostx64/x64/dumpbin.exe" /dependents $i | grep '^[ ].*\.dll' | grep -v 'msvcrt\.dll' | grep -v 'ADVAPI32\.dll' | grep -v 'KERNEL32\.dll' | grep -v 'CRYPT32\.dll' | grep -v 'USER32\.dll' | grep -v 'WS2_32\.dll' | tr -d '\r'); do
-      if [ -f src/nix/$j ]; then
-        continue
-      fi
-      found="$(find {/nix/store,src} -maxdepth 4 -name $j -type f | head -n1)"
-      if [ -z "$found" ]; then
-        continue
-      fi
-      copied=1
-      cp -v "$found" src/nix/ 2>/dev/null
-    done
-  done
-done
+ln -fsv $PWD/src/lib*/libnix*.dll src/nix/
+linkDLLsInfolder src/nix/
