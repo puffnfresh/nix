06c5283a8a754cfb6140ac135edae5798eb47cdf
Brian McKenna <brian@brianmckenna.org>
2025-02-17 21:25:52 +1100

    SourceAccessor::showPath: always has a displayPrefix now, so use rel()

--- src/libutil/source-accessor.cc
+++ src/libutil/source-accessor.cc
@@ -84,7 +84,7 @@ void SourceAccessor::setPathDisplay(std::string displayPrefix, std::string displ
 
 std::string SourceAccessor::showPath(const CanonPath & path)
 {
-    return displayPrefix + path.abs() + displaySuffix;
+    return displayPrefix + path.rel() + displaySuffix;
 }
 
 CanonPath SourceAccessor::resolveSymlinks(
