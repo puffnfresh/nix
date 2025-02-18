461f853bbf99637568e22849723a5aacf26c0659
Brian McKenna <brian@brianmckenna.org>
2025-02-18 14:35:58 +1100

    copy-dlls: improve

--- scripts/copy-dlls.sh
+++ scripts/copy-dlls.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-ln -fsv $PWD/src/lib*/libnix*.dll src/nix/
-linkDLLsInfolder src/nix/
+if [ "$#" = 0 ]
+then
+    echo "Usage: source $BASH_SOURCE <build_dir> <dest>" >&2
+    return 1
+fi
+
+for i in $(find "$1" -name "*.dll" -type f); do
+  ln -fsv "$(realpath $i)" "$2/"
+done
+linkDLLsInfolder "$2"
