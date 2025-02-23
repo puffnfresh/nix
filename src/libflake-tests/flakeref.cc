e2fedc3b319de7b313a92c6731646885bf202671
Brian McKenna <brian@brianmckenna.org>
2025-02-24 01:51:06 +1100

    windows: skip broken test in libflake-tests

--- src/libflake-tests/flakeref.cc
+++ src/libflake-tests/flakeref.cc
@@ -3,6 +3,12 @@
 #include "fetch-settings.hh"
 #include "flake/flakeref.hh"
 
+#ifndef _WIN32
+#define BASE_PATH "/foo/bar"
+#else
+#define BASE_PATH "C:/foo/bar"
+#endif
+
 namespace nix {
 
 /* ----------- tests for flake/flakeref.hh --------------------------------------------------*/
@@ -13,42 +19,42 @@ namespace nix {
         fetchers::Settings fetchSettings;
 
         {
-            auto s = "/foo/bar";
+            auto s = BASE_PATH;
             auto flakeref = parseFlakeRef(fetchSettings, s);
-            ASSERT_EQ(flakeref.to_string(), "path:/foo/bar");
+            ASSERT_EQ(flakeref.to_string(), "path:" BASE_PATH);
         }
 
         {
-            auto s = "/foo/bar?revCount=123&rev=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            auto s = BASE_PATH "?revCount=123&rev=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
             auto flakeref = parseFlakeRef(fetchSettings, s);
-            ASSERT_EQ(flakeref.to_string(), "path:/foo/bar?rev=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&revCount=123");
+            ASSERT_EQ(flakeref.to_string(), "path:" BASE_PATH "?rev=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&revCount=123");
         }
 
         {
-            auto s = "/foo/bar?xyzzy=123";
+            auto s = BASE_PATH "?xyzzy=123";
             EXPECT_THROW(
                 parseFlakeRef(fetchSettings, s),
                 Error);
         }
 
         {
-            auto s = "/foo/bar#bla";
+            auto s = BASE_PATH "#bla";
             EXPECT_THROW(
                 parseFlakeRef(fetchSettings, s),
                 Error);
         }
 
         {
-            auto s = "/foo/bar#bla";
+            auto s = BASE_PATH "#bla";
             auto [flakeref, fragment] = parseFlakeRefWithFragment(fetchSettings, s);
-            ASSERT_EQ(flakeref.to_string(), "path:/foo/bar");
+            ASSERT_EQ(flakeref.to_string(), "path:" BASE_PATH);
             ASSERT_EQ(fragment, "bla");
         }
 
         {
-            auto s = "/foo/bar?revCount=123#bla";
+            auto s = BASE_PATH "?revCount=123#bla";
             auto [flakeref, fragment] = parseFlakeRefWithFragment(fetchSettings, s);
-            ASSERT_EQ(flakeref.to_string(), "path:/foo/bar?revCount=123");
+            ASSERT_EQ(flakeref.to_string(), "path:" BASE_PATH "?revCount=123");
             ASSERT_EQ(fragment, "bla");
         }
     }
