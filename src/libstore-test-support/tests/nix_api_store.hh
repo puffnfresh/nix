ce03ef7af444e5ee9d5e64b871f854951a0dbb67
Brian McKenna <brian@brianmckenna.org>
2025-02-24 01:51:47 +1100

    windows: fix creating test store on Windows

--- src/libstore-test-support/tests/nix_api_store.hh
+++ src/libstore-test-support/tests/nix_api_store.hh
@@ -44,8 +44,8 @@ protected:
         // no `mkdtemp` with MinGW
         auto tmpl = nix::defaultTempDir() + "/tests_nix-store.";
         for (size_t i = 0; true; ++i) {
-            nixDir = tmpl + std::string { i };
-            if (fs::create_directory(nixDir)) break;
+            nixDir = tmpl + std::to_string(i);
+            if (fs::create_directories(nixDir)) break;
         }
 #else
         // resolve any symlinks in i.e. on macOS /tmp -> /private/tmp
